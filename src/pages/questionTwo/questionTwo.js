3c06f34dfc5abc2f7a7587d839e869e81e0df241
th3N0m4d <edielton.dantas@hotmail.com>
2020-04-01 11:49:37 +0200

    Question #2: Refactoring and a bit of cleaning up

--- src/pages/questionTwo/questionTwo.js
+++ src/pages/questionTwo/questionTwo.js
@@ -1,65 +1,62 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
 import { withStyles } from '@material-ui/core/styles'
-
+import Async from 'react-async'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
 import { getCardDetails } from './api'
 import styles from './styles'
+import { Container } from './atoms'
 
-class QuestionTwo extends Component {
-	state = {
-		title: null,
-		imgSrc: null,
-		body: '',
-		loading: true
-	}
-	
-	componentDidMount() {
-		
-		getCardDetails().then(({ title, imgSrc, body}) => {
-		  this.setState({
-        title,
-        imgSrc,
-        body,
-        loading: false
-		  })
-		})
-		
-	}  
-
+class QuestionTwo extends PureComponent {
   render () {
     const { classes } = this.props
-    const { title, imgSrc, body, loading } = this.state
-    if (loading) {
-      return (
-        <div className={classes.spinner}>
-          <CircularProgress />
-        </div>
-      )
-    }
+
     return (
-      <div className={classes.container}>
-        <Card className={classes.card}>
-          <CardMedia
-            className={classes.media}
-            image={imgSrc}
-            title={title}
-          />
-          <CardContent className={classes.content}>
-            <Typography gutterBottom variant='h5' component='h2'>
-              {title}
-            </Typography>
-            <div
-              className={classes.body}
-              dangerouslySetInnerHTML={{ __html: body }}
-            />
-          </CardContent>
-        </Card>
-      </div>
+
+      <Async promiseFn={getCardDetails}>
+        <Async.Loading>
+          <div className={classes.spinner}>
+            <CircularProgress />
+          </div>
+        </Async.Loading>
+        <Async.Resolved>
+          {
+            ({ title, imgSrc, body }) => (
+              <div className={classes.container}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.media}
+                    image={imgSrc}
+                    title={title}
+                  />
+                  <CardContent className={classes.content}>
+                    <Typography gutterBottom variant='h5' component='h2'>
+                      {title}
+                    </Typography>
+                    <div
+                      className={classes.body}
+                      dangerouslySetInnerHTML={{ __html: body }}
+                    />
+                  </CardContent>
+                </Card>
+              </div>
+            )
+          }
+        </Async.Resolved>
+        <Async.Rejected>
+          {
+            error => (
+              <Container>
+                {error.message}
+              </Container>
+            )
+          }
+        </Async.Rejected>
+      </Async>
     )
   }
 }
