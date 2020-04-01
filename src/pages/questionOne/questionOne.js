ff68ca47252fa7a3b1bc34ce567ed70586fd066f
th3N0m4d <edielton.dantas@hotmail.com>
2020-04-01 08:30:02 +0200

    Question #1: Remove unecessary label state and adopt HOC approach for index

--- src/pages/questionOne/questionOne.js
+++ src/pages/questionOne/questionOne.js
@@ -1,30 +1,14 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import Button from '@material-ui/core/Button'
 
 import { Container } from './atoms'
 
-class QuestionOne extends PureComponent {
-
-	state = {
-		label: "I've been clicked: ",
-		counter: 0
-	}
-
-  handleOnClick = () => {
-    this.setState((prevState) => ({
-      counter: prevState.counter + 1
-    }))
-  }
-
-  render () {
-    return (
-      <Container>
-        <Button variant='contained' onClick={this.handleOnClick}>
-          {this.state.label} {this.state.counter} times
-        </Button>
-      </Container>
-    )
-  }
-}
+const QuestionOne = ({ count, onClick }) => (
+  <Container>
+    <Button variant='contained' onClick={onClick}>
+      {`I've been clicked: ${count} times`}
+    </Button>
+  </Container>
+)
 
 export default QuestionOne
